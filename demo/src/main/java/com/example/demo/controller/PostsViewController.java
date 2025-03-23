3f2c5c37229babee679c71d1c3de2627d7a5de07
denchik2245 <deniszak54321@gmail.com>
2025-03-23 16:55:13 +0500

    View

--- demo/src/main/java/com/example/demo/controller/PostsViewController.java
+++ demo/src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostsViewController {
-    @ResponseBody
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        return "list";
     }
 
     @ResponseBody
