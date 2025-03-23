c26cac54870fac0abb4dc84f47466e95bd823b83
denchik2245 <deniszak54321@gmail.com>
2025-03-23 17:00:34 +0500

    Bootstrap

--- demo/src/main/java/com/example/demo/model/Post.java
+++ demo/src/main/java/com/example/demo/model/Post.java
@@ -1,12 +1,23 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post (String text, Integer likes) {
+    public Post (String text, Date creationDate) {
         this.text = text;
-        this.likes = likes;
+        this.creationDate = creationDate;
+    }
+
+    public Post (String text){
+        this.text = text;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 
     public String getText() {
--- demo/src/main/java/com/example/demo/service/PostService.java
+++ demo/src/main/java/com/example/demo/service/PostService.java
@@ -4,16 +4,16 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
-
+    List<Post> list = new ArrayList<>();
     public List<Post> listAllPosts() {
-        List<Post> list = new ArrayList<>();
-        list.add(new Post("First post", 111));
-        list.add(new Post("Second post", 222));
-        list.add(new Post("Third post", 333));
+        list.add(new Post("First post",new Date()));
+        list.add(new Post("Second post", new Date()));
+        list.add(new Post("Third post", new Date()));
         return list;
     }
 }
